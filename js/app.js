d13e9939c9166bc0f558abf830e4643de563379b
Railane Passos <railanepassos@gmail.com>
2020-06-22 16:51:38 -0300

    Ajustando comentários no código.

--- js/app.js
+++ js/app.js
@@ -1,23 +1,16 @@
 
-console.log("Isso é um codigo Js dentro da página!");
-
-//function Cadastrar(){
-    //  Buscando elemento por Id
-//    var gameName = document.getElementById("gameId");
-
-    //  Imprime no console o valor dentro do campo "Nome do Jogo" ao clicar no botão "Cadastrar"
-//    console.log(gameName.value);
-
-    //  Regra de Negócio:  Não deve ser cadastrado jogos com mesmas informações
-    //  Regra de Negocio:  Todos os campos são obrigatórios e devem ser preenchidos
-//    if(gameName.value == "") {
-//        alert("Nome do jogo deve ser preenchido !")
-//    } else {
-//        alert("Os dados foram encaminhados com sucesso !")
-//    }
-//}///
-
-
+//  Aqui é ultilizado propriedade onload para que o javascript seja carregado após a pagina HTML.
+//  Ao criar a pagina HTML o javascript foi linkado em seu Header, com isso os elementos da pagina 
+//      estavam sendo carregados após o JavaScript, retornando erro. Para correção do incidente foi 
+//      ultilizado a propriedade onload que carrega após o processo de carregamento do documento. 
+//      https://pt.stackoverflow.com/questions/207822/cannot-read-property-addeventlistener-of-null/207824?noredirect=1#comment872899_207824
+//  Uma outra dúvida que surgiu nessa parte é em relação a "Onde colocar o link para o fim do Js?". 
+//  Podemos colocar um <script> no cabeçalho ou corpo do html (<head> ou <body>) a depender do que ele faz. Ao colocar
+//      no cabeçalho ele será carregado antes do carregamento do documento, para que isso não aconteça
+//      basta fazer uso da propriedade onload. Ao colocar o <script> ao fim do <body> pode passar a 
+//      impressão de uma pagina mais "rápida" porem se a mesma for muito dependente do script em seu design
+//      a mesma parecerá "desconfigurada".
+//      https://pt.stackoverflow.com/questions/1109/onde-devo-colocar-um-c%C3%B3digo-javascript-em-um-documento-html
 
 window.onload = function(){
     //  Usando Seletor CSS para identificar/ buscar o elemento.
